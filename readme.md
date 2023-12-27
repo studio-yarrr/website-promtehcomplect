15ee385ea100e550113b054d26843a834c8f6f10
KonstantinVolodko <faker2000311@gmail.com>
2023-12-27 18:12:48 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+https://studio-yarrr.github.io/website-promtehcomplect/dist/
+
+[Pages](https://studio-yarrr.github.io/website-promtehcomplect/dist/)
+[Figma](https://www.figma.com/file/X6bASBbgbIteiA4e9fe3jF/АО-«ПРОМТЕХКОМПЛЕКТ»-(Copy)?mode=dev)
+
+
---
+++ src/assets/js/app.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //= components/
+
+    Fancybox.bind("[data-fancybox]", {
+        Thumbs: false
+    });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    SmoothScroll({
+        // Время скролла 400 = 0.4 секунды
+        animationTime: 800,
+        // Размер шага в пикселях 
+        stepSize: 75,
+
+        // Дополнительные настройки:
+
+        // Ускорение 
+        accelerationDelta: 30,
+        // Максимальное ускорение
+        accelerationMax: 2,
+
+        // Поддержка клавиатуры
+        keyboardSupport: true,
+        // Шаг скролла стрелками на клавиатуре в пикселях
+        arrowScroll: 50,
+
+        // Pulse (less tweakable)
+        // ratio of "tail" to "acceleration"
+        pulseAlgorithm: true,
+        pulseScale: 4,
+        pulseNormalize: 1,
+
+        // Поддержка тачпада
+        touchpadSupport: true,
+    })
+
+
+    gsap.to(".ticker-content", {
+        xPercent: 100,
+        ease: "none",
+        repeat: -1,
+        duration: 10
+    });
+
+    if (document.querySelector('.video-container button')) {
+        document.querySelector('.video-container button').addEventListener('click', function() {
+            var video = document.querySelector('.video-container video');
+            video.play();
+            video.setAttribute('controls', '');
+            this.style.display = 'none';
+        });
+    }
+
+    document.querySelectorAll('.increase, .decrease').forEach(function(button) {
+        button.addEventListener('click', function() {
+            const input = this.parentNode.querySelector('.number-input');
+            const currentValue = parseInt(input.value, 10);
+            if (this.classList.contains('increase')) {
+                input.value = currentValue + 1;
+            } else {
+                input.value = currentValue - 1;
+            }
+        });
+    });
+    
+
+});
+
+
+
